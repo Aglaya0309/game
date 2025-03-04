d5ffc1331c6ea4aa1969e839b81f30f6afbb9f47
ElviraKondratova <elvirochka_sh@mail.ru>
2025-03-04 18:30:19 +0300

    add all const

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/tir.jpg")
 pygame.display.set_icon(icon)
 
-target_image = pygame.image.load("img/tir.jpg")
+target_image = pygame.image.load("img/monster.jpg")
 
 target_width = 80
 target_height = 80
